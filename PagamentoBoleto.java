92659c0d6523408c5a1c5728ac891886d590806f
Bruno Muraro <88937060+Bmuraro@users.noreply.github.com>
2021-09-22 15:42:53 -0300

    Incluindo a interface Serializable

--- PagamentoBoleto.java
+++ PagamentoBoleto.java
@@ -1,8 +1,11 @@
 package projetoIntegrador;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-public class PagamentoBoleto extends Pagamento {
+public class PagamentoBoleto extends Pagamento implements Serializable {
+	    private  static final long serialVersionUID = 1L;
 
 	Date dataPagamento;
 	Date dataVencimento;
@@ -34,8 +37,30 @@ public class PagamentoBoleto extends Pagamento {
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(dataPagamento, dataVencimento);
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PagamentoBoleto other = (PagamentoBoleto) obj;
+		return Objects.equals(dataPagamento, other.dataPagamento)
+				&& Objects.equals(dataVencimento, other.dataVencimento);
+	}
 	
 	
 	
 }
-
